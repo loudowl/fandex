287d59cc7a44d1ed5ab1dedbd1a1b879e5f6a998
Chris <loudowl@mac.com>
2026-07-04 13:54:06 -0700

    feat: initial implementation

---
+++ fandex-frontend/utils/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#212529',
+    color: '#F8F9FA',
+  },
+  text: {
+    color: '#F8F9FA',
+  },
+});
