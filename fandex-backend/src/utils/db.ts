9de122c466771248521da4a7a127c5aa3584dd23
Chris <loudowl@mac.com>
2026-07-04 13:54:15 -0700

    feat: initial implementation

---
+++ fandex-backend/src/utils/db.ts
@@ -0,0 +1,7 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+});
